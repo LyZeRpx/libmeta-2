6aaabd383fd5cd9e482f093eaa900255c455e1ba
liam harfouche <liam.harfouche@epitech.eu>
2024-05-27 16:51:03 +0200

    [ADD]: meta link repo structures and structs in header

---
+++ modules/meta_links/headers/meta_links.h
@@ -0,0 +1,21 @@
+/*
+** EPITECH PROJECT, 2024
+** new_project
+** File description:
+** project.h
+*/
+
+#ifndef META_LINKS_H
+    #define META_LINKS_H
+
+typedef struct meta_linked_list_s
+{
+    void *data;
+    struct meta_linked_list_s *next;
+}meta_linked_list_t;
+
+meta_linked_list_t *meta_new_list(void);
+void meta_add_to_list(meta_linked_list_t *list, void *data);
+void meta_dump_list(meta_linked_list_t list);
+
+#endif
