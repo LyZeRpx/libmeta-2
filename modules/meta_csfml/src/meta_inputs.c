f9f2639970f30e03aad34dac805070bac8c14f49
liam harfouche <liam.harfouche@epitech.eu>
2024-05-28 17:45:35 +0200

    [EDIT]: window names in input.c

--- modules/meta_csfml/src/meta_inputs.c
+++ modules/meta_csfml/src/meta_inputs.c
@@ -7,9 +7,9 @@
 
 #include "meta_utils.h"
 
-sfVector2i meta_track_mouse_inbounds(sfRenderWindow *window)
+sfVector2i meta_track_mouse_inbounds(sfRenderWindow *relative_to)
 {
-    return sfMouse_getPositionRenderWindow(window);
+    return sfMouse_getPositionRenderWindow(relative_to);
 }
 
 sfMouseButton meta_get_mouse_click(void)
@@ -20,13 +20,13 @@ sfMouseButton meta_get_mouse_click(void)
     return i;
 }
 
-sfVector2i meta_get_click_position(sfRenderWindow *window)
+sfVector2i meta_get_click_position(sfRenderWindow *relative_to)
 {
     if (meta_get_mouse_click() != -1);
-        return meta_track_mouse_inbounds(window);
+        return meta_track_mouse_inbounds(relative_to);
 }
 
-sfKeyCode meta_get_key_input()
+sfKeyCode meta_get_key_input(void)
 {
     sfKeyCode i = 0;
 
