3d334b85f5feddbcd88983ade36aadbfc9de7bd6
liam harfouche <liam.harfouche@epitech.eu>
2024-07-11 05:00:35 +0200

    [FIX]: click and track

--- modules/meta_csfml/src/meta_detection.c
+++ modules/meta_csfml/src/meta_detection.c
@@ -18,7 +18,7 @@ sfBool meta_detect_collision(meta_assets_t object_1, meta_assets_t object_2)
 
     object_1.position = sfSprite_getPosition(object_1.asset);
     object_2.position = sfSprite_getPosition(object_2.asset);
-   
+
    return X_DIFF <= RADIUS_SUM_X || Y_DIFF <= RADIUS_SUM_Y ? sfTrue : sfFalse;
 
 }
