91bfbaa2baec6038eb8f3caddba17f88705123cc
Liam Harfouche <liam.harfouche@epitech.eu>
2024-09-26 15:09:04 +0200

    [FIX]: pm renamed ranks to fit desciption

--- modules/meta_libc/src/meta_promote_rank1.c
+++ modules/meta_libc/src/meta_promote_rank1.c
@@ -24,22 +24,18 @@ static int count_limiters(char *str, char target)
 
 char **meta_promote_str(char *str, const char limiter)
 {
-    char **word_array = NULL;
-    int y = 0;
-    int x = 0;
-    int i = 0;
+    char **word_array = malloc(sizeof(char *) * count_limiters(str, limiter) + 1);
+    char buffer[256];
+    size_t j = 0;
+    size_t k = 0;
 
-    word_array = malloc(sizeof(char *) * (count_limiters(str, limiter) + 1));
-    for (; y != count_limiters(str, limiter); y++) {
-        word_array[y] = malloc((sizeof(char) * (meta_strlen(str) + 1)));
-        for (; str[i] != limiter && str[i] != '\0'; i++) {
-            word_array[y][x] = str[i];
-            x++;
+    for (; word_array[k]; k++) {
+        for (size_t i = 0; str[i] != limiter; i++) {
+            str[i] = buffer[j];
         }
-        for (; str[i] == limiter; i++);
-        word_array[y][x] = '\0';
-        x = 0;
+        j = 0;
+        word_array[k] = meta_strdup(buffer);
     }
-    word_array[y] = NULL;
+    word_array[k + 1] = NULL;
     return word_array;
 }
