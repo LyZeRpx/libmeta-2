ab6be96fd580522ed48d25807113604758750a70
Liam Harfouche <liam.harfouche@epitech.eu>
2024-10-13 22:42:32 +0200

    [UPDATE] : meta_is_prime

--- modules/meta_libc/src/meta_is_prime.c
+++ modules/meta_libc/src/meta_is_prime.c
@@ -1,19 +1,19 @@
 /*
 ** EPITECH PROJECT, 2024
-** I AM...
+** MY_PROJECT
 ** File description:
-** OPTIMUM PRIDE
+** DESCRIPTION
 */
 
-int meta_is_prime(int nb)
+#include <stdio.h>
+
+int my_is_prime(int nb)
 {
-    if (nb == 0 || nb == 1) {
-            return 1;
-        }
+    if (nb < 0 || nb == 0 || nb == 1)
+        return 0;
     for (int i = 2; i < nb; i++) {
-        if (nb % i == 0) {
-        return 1;
-        }
+        if (nb % i == 0)
+            return 0;
     }
-    return 0;
+    return 1;
 }
